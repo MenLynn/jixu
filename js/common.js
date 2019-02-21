a78dba890ae3779db59ed2caf09aa9fe9c3a0776
王梦丽 <15713895059@sina.cn>
2019-02-21 16:22:28 +0800

    新闻中心 && 常见问题

--- js/common.js
+++ js/common.js
@@ -4,20 +4,26 @@ $(function () {
   $(".footer-box").load("footer.html");
 
   var windowWidth;
+  var $itemlf = $('.detail-img');
 
   function init() {
     windowWidth = window.innerWidth;
     $('.nav-content').width(windowWidth - 120);
+    $('.section').css('min-height', window.innerHeight + 'px');
 
     if (windowWidth > 768) {
       $('.nav-box').show();
-      $('.header').css('left','120px');
-      $('.section').css('margin-left','120px');
+      $('.header').css('left', '120px');
+      $('.section').css('margin-left', '120px');
+      $itemlf.width($('.news-detail').width() * 0.46);
     } else {
       $('.nav-box').hide();
-      $('.header').css('left','0');
-      $('.section').css('margin','0');
+      $('.header').css('left', '0');
+      $('.section').css('margin', '0');
+      $itemlf.width($('.news-detail').width());
     }
+
+    $itemlf.height($itemlf.width() * 0.67);
   }
 
   setTimeout(function () {
